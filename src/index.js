ba262391af64e900b59acf97d5dfa28b15b7ee46
fraander <57777918+fraander@users.noreply.github.com>
2023-02-17 11:08:40 -0500

    testing src rendering bugs

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <h1>Test</h1>
   </React.StrictMode>
 );
 
