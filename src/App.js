98b64567b451fb3d0e038e545d165b44535a30e3
fraander <57777918+fraander@users.noreply.github.com>
2023-02-22 20:12:12 -0500

    hello world reducer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Labs from "./labs"
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router"
+import {Routes, Route} from "react-router"
 
 function App() {
     return (
