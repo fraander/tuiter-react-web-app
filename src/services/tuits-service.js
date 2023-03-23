313fb491e45b05a86d2db0e2ec8152bdd9e56dd4
fraander <57777918+fraander@users.noreply.github.com>
2023-03-22 20:57:12 -0400

    change link

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_LINK = 'https://tuiter-node-server-app-frank.herokuapp.com/api'
+const SERVER_LINK = 'https://tuiter-node-server-app-cbag.onrender.com/'
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE ?? SERVER_LINK}/tuits`;
 
